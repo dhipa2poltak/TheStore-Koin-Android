df48a26baabbc670b7e168b69423f97def928ee7
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-02-25 14:38:57 +0700

    updating unit test for RemoteDataSource

--- framework/src/test/java/com/dpfht/thestore_koin/data/datasource/remote/RemoteDataSourceTest.kt
+++ framework/src/test/java/com/dpfht/thestore_koin/data/datasource/remote/RemoteDataSourceTest.kt
@@ -5,10 +5,13 @@ import com.dpfht.thestore_koin.data.model.remote.Data
 import com.dpfht.thestore_koin.data.model.remote.DataResponse
 import com.dpfht.thestore_koin.data.model.remote.Product
 import com.dpfht.thestore_koin.data.model.remote.toDomain
+import com.dpfht.thestore_koin.domain.entity.AppException
 import com.dpfht.thestore_koin.framework.data.datasource.remote.RemoteDataSourceImpl
 import com.dpfht.thestore_koin.framework.data.datasource.remote.rest.RestService
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -16,6 +19,8 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
+import retrofit2.HttpException
+import retrofit2.Response
 
 @RunWith(MockitoJUnitRunner::class)
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -42,5 +47,41 @@ class RemoteDataSourceTest {
     val actual = remoteDataSource.getProducts()
 
     assertTrue(dataResponse.toDomain() == actual)
+  }
+
+  @Test
+  fun `failed fetch products`() = runTest {
+    whenever(restService.getProducts()).then {
+      throw Exception()
+    }
+
+    var actual = ""
+    try {
+      remoteDataSource.getProducts()
+    } catch (e: AppException) {
+      actual = e.message
+    }
+
+    assertTrue(actual.isNotEmpty())
+  }
+
+  @Test
+  fun `when fetching products API, it is generating an HTTPException `() = runTest {
+    val msg = "This is an error message"
+
+    whenever(restService.getProducts()).then {
+      val body = msg.toResponseBody("application/text".toMediaTypeOrNull())
+      val r = Response.error<String>(400, body)
+      throw HttpException(r)
+    }
+
+    var actual = ""
+    try {
+      remoteDataSource.getProducts()
+    } catch (e: AppException) {
+      actual = e.message
+    }
+
+    assertTrue(actual.isNotEmpty())
   }
 }
